1bcd941071595c1489ceea964a1a85cd0dfd54c7
dev-v <250912570@qq.com>
2018-02-23 19:26:14 +0800

    提交

--- android/app/src/main/java/com/dbercustomerapp/MainApplication.java
+++ android/app/src/main/java/com/dbercustomerapp/MainApplication.java
@@ -3,11 +3,6 @@ package com.dbercustomerapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.horcrux.svg.SvgPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.dylanvann.fastimage.FastImageViewPackage;
-import cn.qiuxiang.react.amap3d.AMap3DPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,12 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new SvgPackage(),
-            new LinearGradientPackage(),
-            new FastImageViewPackage(),
-            new AMap3DPackage()
+          new MainReactPackage()
       );
     }
 
